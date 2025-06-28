c0049e7bd206392d437187b743f686b870bdc7a1
Vansh Yelekar <vanshyelekar04@gmail.com>
2025-06-28 20:00:49 +0530

    Update manifest for PWA and dashboard branding

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,9 @@ const PORT = process.env.PORT || 5000;
 // 🔐 Security Middleware
 app.use(helmet());
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://financial-dashboard-ivory-pi.vercel.app','https://financial-dashboard-git-main-vanshs-projects-15abcf20.vercel.app/'],
+  origin: ['http://localhost:3000', 'https://financial-dashboard-ivory-pi.vercel.app','https://financial-dashboard-git-main-vanshs-projects-15abcf20.vercel.app/',
+    'https://financial-dashboard-d9dfrwscl-vanshs-projects-15abcf20.vercel.app'
+  ],
   credentials: true,
 }));
 app.use(express.json({ limit: '10kb' }));
