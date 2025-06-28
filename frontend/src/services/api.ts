342e16b6f782def9f98bc530d10ee660288d3bf4
Vansh Yelekar <vanshyelekar04@gmail.com>
2025-06-28 14:05:06 +0530

    fix: credentials

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL+ '/api',
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 // Attach JWT token from localStorage to every request
